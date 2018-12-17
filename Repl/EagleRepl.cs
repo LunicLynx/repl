66a8fbdf00a7c1b88c2104848d5f5db537e61cd1
Florian Dohrendorf <git+repl@fdohrendorf.net>
2018-12-17 23:57:43 +0100

    start better repl

---
+++ Repl/EagleRepl.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Repl.CodeAnalysis;
+using Repl.CodeAnalysis.CodeGen;
+using Repl.CodeAnalysis.Syntax;
+using Repl.CodeAnalysis.Text;
+
+namespace Repl
+{
+    public class EagleRepl : Repl
+    {
+        private Compilation _previous;
+        private bool _showTree;
+        private bool _showProgram = true;
+        private bool _compile;
+        private readonly Dictionary<VariableSymbol, object> _variables = new Dictionary<VariableSymbol, object>();
+
+        private readonly Compiler _compiler = new Compiler();
+
+        protected override void EvaluateMetaCommand(string input)
+        {
+            switch (input)
+            {
+                case "#showTree":
+                    _showTree = !_showTree;
+                    Console.WriteLine(_showTree ? "Showing parse trees." : "Not showing parse trees.");
+                    break;
+                case "#showProgram":
+                    _showProgram = !_showProgram;
+                    Console.WriteLine(_showProgram ? "Showing bound trees." : "Not showing bound trees.");
+                    break;
+                case "#cls":
+                    Console.Clear();
+                    break;
+                case "#reset":
+                    _previous = null;
+                    break;
+                case "#emit":
+                    _compile = !_compile;
+                    Console.WriteLine(_showTree ? "Emit binary." : "Not emitting binary.");
+                    break;
+                default:
+                    base.EvaluateMetaCommand(input);
+                    break;
+            }
+        }
+
+        protected override bool IsCompleteSubmission(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return false;
+
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            return !syntaxTree.Diagnostics.Any();
+        }
+
+        protected override void EvaluateSubmission(string text)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);
+
+            var compilation = _previous == null
+                ? new Compilation(syntaxTree)
+                : _previous.ContinueWith(syntaxTree);
+
+            if (_showTree)
+            {
+                var printer = new Printer();
+                printer.Print(syntaxTree.Root.Statement);
+            }
+
+            if (_showProgram)
+            {
+                var printer = new Printer();
+                printer.Print(compilation);
+            }
+
+            var result = compilation.Evaluate(_variables);
+
+            if (!result.Diagnostics.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(result.Value);
+                Console.ResetColor();
+
+                // TODO if (compile) - if activated we would need to re-emit the whole module
+                if (_compile)
+                    _compiler.CompileAndRun(compilation, _variables);
+
+                _previous = compilation;
+            }
+            else
+            {
+                var text1 = syntaxTree.Text;
+
+                foreach (var diagnostic in result.Diagnostics)
+                {
+                    var lineIndex = text1.GetLineIndex(diagnostic.Span.Start);
+                    var line = text1.Lines[lineIndex];
+                    var lineNumber = lineIndex + 1;
+                    var character = diagnostic.Span.Start - line.Start + 1;
+
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($"({lineNumber}, {character}): ");
+                    Console.WriteLine(diagnostic);
+                    Console.ResetColor();
+
+                    var prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                    var suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                    var prefix = text1.ToString(prefixSpan);
+                    var error = text1.ToString(diagnostic.Span);
+                    var suffix = text1.ToString(suffixSpan);
+
+                    Console.Write("    ");
+                    Console.Write(prefix);
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(error);
+                    Console.ResetColor();
+
+                    Console.Write(suffix);
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Repl/Repl.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace Repl
+{
+    public abstract class Repl
+    {
+
+        public void Run()
+        {
+            while (true)
+            {
+                var text = EditSubmission();
+                if (text == null)
+                    return;
+
+                EvaluateSubmission(text);
+            }
+        }
+
+        private string EditSubmission()
+        {
+            var textBuilder = new StringBuilder();
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(textBuilder.Length == 0 ? "» " : "· ");
+                Console.ResetColor();
+
+                var input = Console.ReadLine();
+                var isBlank = string.IsNullOrWhiteSpace(input);
+
+                if (textBuilder.Length == 0)
+                {
+                    if (isBlank)
+                        return null;
+
+                    if (input.StartsWith("#"))
+                    {
+                        EvaluateMetaCommand(input);
+                        continue;
+                    }
+                }
+
+                textBuilder.AppendLine(input);
+                var text = textBuilder.ToString();
+
+                if (!IsCompleteSubmission(text))
+                    continue;
+
+                return text;
+            }
+        }
+
+        protected virtual void EvaluateMetaCommand(string input)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Invalid command {input}");
+            Console.ResetColor();
+        }
+
+        protected abstract bool IsCompleteSubmission(string text);
+
+        protected abstract void EvaluateSubmission(string text);
+    }
+}
